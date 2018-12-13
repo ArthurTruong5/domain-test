b4238d5c0d2895e839dfeef8210fac4833415faa
arthurtruong <arthurtruong.work@gmail.com>
2018-12-14 01:44:27 +1100

    first commit

---
+++ src/components/main/Home.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import { Container, Divider } from "semantic-ui-react";
+
+class Home extends Component {
+  render() {
+    return (
+      <div>
+        <Container textAlign="left">test</Container>
+        <Container textAlign="right">test</Container>
+      </div>
+    );
+  }
+}
+
+export default Home;
