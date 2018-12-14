010bb80ee775ab2758d75d3a0888c83fe16f64be
arthurtruong <arthurtruong.work@gmail.com>
2018-12-14 20:36:14 +1100

    business card

--- src/components/card/BusinessCard.js
+++ src/components/card/BusinessCard.js
@@ -1,27 +1,46 @@
 import React, { Component } from "react";
 import "./BusinessCard.scss";
 import BusinessCardTextGroup from "../common/BusinessCardTextGroup";
-import { Grid, Divider, Card, Icon } from "semantic-ui-react";
+import { Grid, Divider, Card, Icon, Container } from "semantic-ui-react";
+import CardPreviewTextGroup from "../common/CardPreviewTextGroup";
 
 class BusinessCard extends Component {
   render() {
     return (
-      <section className="business-card">
-        <h4 className="text-preview">HCARD PREVIEW</h4>
-        <div className="whole-card">
-          <div className="name">
-            <h2>Arthur Truong</h2>
-          </div>
-        </div>
-        <div className="card-form">
-          <div className="card-fill">
-            <h4 className="card-text">EMAIL:</h4>
-            <div className="card-divider">
-              <hr />
+      <Container>
+        <section className="business-card">
+          <h4 className="text-preview">HCARD PREVIEW</h4>
+          <div className="whole-card">
+            <div className="name">
+              <h2>Arthur Truong</h2>
             </div>
           </div>
-        </div>
-      </section>
+        </section>
+        <section className="business-input">
+          <div className="whole-body">
+            <Container className="name-input">
+              <Grid columns={1} divided>
+                <Grid.Row>
+                  <Grid.Column>
+                    <h4>NAME</h4>
+                    <Divider />
+                  </Grid.Column>
+                </Grid.Row>
+                <Grid.Row>
+                  <Grid.Column>
+                    <h4>NAME</h4>
+                  </Grid.Column>
+                </Grid.Row>
+                <Grid.Row>
+                  <Grid.Column>
+                    <h4>NAME</h4>
+                  </Grid.Column>
+                </Grid.Row>
+              </Grid>
+            </Container>
+          </div>
+        </section>
+      </Container>
     );
   }
 }
